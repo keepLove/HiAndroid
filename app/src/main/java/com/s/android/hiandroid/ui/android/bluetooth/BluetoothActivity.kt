ed4894c98d98e7c994f55a7b233ad9d707f78b6c
Administrator <shuaijianwen@jadegold.cn>
2019-04-05 19:21:33 +0800

    蓝牙

---
+++ app/src/main/java/com/s/android/hiandroid/ui/android/bluetooth/BluetoothActivity.kt
@@ -0,0 +1,13 @@
+package com.s.android.hiandroid.ui.android.bluetooth
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.s.android.hiandroid.R
+
+class BluetoothActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth)
+    }
+}
